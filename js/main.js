f8ef3b17d855ce63041dbf0bda3b90a2105175a5
BaraAlOmari <60300383@udst.edu.qa>
2025-08-04 12:27:15 +0300

    Added Sticky navbar & pop-up project previews

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ window.addEventListener('scroll', () => {
 backToTopBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('backdrop-blur-md', 'bg-stone-900/70', 'p-12', 'rounded-3xl', 'transition-all', 'duration-500');
+    } else {
+        navbar.classList.remove('backdrop-blur-md', 'bg-stone-900/70', 'p-12', 'rounded-3xl');
+    }
+});
